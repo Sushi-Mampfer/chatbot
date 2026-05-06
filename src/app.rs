5433b69dee7bf4160832f81b6740751309704cd2
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2026-05-06 14:54:58 +0200

    error

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,12 @@
-use leptos::prelude::*;
+use leptos::{
+    leptos_dom::logging::console_debug_log, prelude::*, server::codee::string::FromToStringCodec,
+};
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet};
 use leptos_router::{
     components::{Route, Router, Routes},
     StaticSegment,
 };
+use leptos_use::storage::use_local_storage;
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
@@ -47,14 +50,48 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
+    let (connected, set_connected) = signal(false);
+    let (id, set_id, remove_id) = use_local_storage::<String, FromToStringCodec>("id");
+    let message = RwSignal::new("".to_string());
+    let (count, set_count) = signal(0);
+
     view! {
+        <button
+            on:click=move |_| set_count.set(3)
+        >
+            "Click me: "
+            {count}
+        </button>
+        <p>
+            "Double count: "
+            {move || count.get() * 2}
+        </p>
         <div id="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-circle-icon lucide-message-circle"><path d="M2.992 16.342a2 2 0 0 1 .094 1.167l-1.065 3.29a1 1 0 0 0 1.236 1.168l3.413-.998a2 2 0 0 1 1.099.092 10 10 0 1 0-4.777-4.719"/></svg></div>
         <div id="chat">
-            <div id="banner"><p>BYOAT Chatbot</p><div id="status"></div></div>
-            <div id="messages"></div>
+            <div id="banner">
+                <p>BYOAT Chatbot</p>
+                <div
+                    class:green=move || connected.get()
+                    id="status"
+                ></div>
+            </div>
+            <div id="messages">
+                <div class="message user">{"What's your most expensive product."}</div>
+                <div class="message bot">Our Heart-Rythm roulette.</div>
+            </div>
             <div id="inputs">
-            <input id="text"></input>
-            <button id="send"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send-icon lucide-send"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"/><path d="m21.854 2.147-10.94 10.939"/></svg></button>
+                <textarea
+                    bind:value=message
+                    id="text"
+                ></textarea>
+                <button
+                    on:click = move |_| {
+                        console_debug_log("test");
+                        console_debug_log(&id.get());
+                        set_id.set(message.get())
+                    }
+                    id="send"
+                ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send-icon lucide-send"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"/><path d="m21.854 2.147-10.94 10.939"/></svg></button>
             </div>
         </div>
     }
