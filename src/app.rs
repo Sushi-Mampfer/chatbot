333f5d1244dc5954da4b25a8beca41f42bf09015
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2026-05-14 23:07:42 +0200

    styling

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use leptos::{
-    logging::debug_log,
+    html,
     prelude::*,
     server::codee::string::{FromToStringCodec, JsonSerdeCodec},
 };
@@ -12,6 +12,7 @@ use leptos_use::{
     core::ConnectionReadyState, storage::use_local_storage, use_websocket, UseWebSocketReturn,
 };
 use serde::{Deserialize, Serialize};
+use web_sys::{ScrollBehavior, ScrollToOptions};
 
 #[derive(Serialize, Deserialize, Clone, PartialEq)]
 pub struct Message {
@@ -66,6 +67,8 @@ fn HomePage() -> impl IntoView {
     let (id, set_id, remove_id) = use_local_storage::<String, FromToStringCodec>("id");
     let input = RwSignal::new("".to_string());
 
+    let messages_container: NodeRef<html::Div> = NodeRef::new();
+
     let url = format!("/api/chat?id={}", id.get()).leak();
     let UseWebSocketReturn {
         ready_state,
@@ -75,30 +78,48 @@ fn HomePage() -> impl IntoView {
     } = use_websocket::<String, Vec<Message>, JsonSerdeCodec>(url);
 
     Effect::new(move || {
-        let _ = message.get();
-        debug_log!("Why does this work");
-    });
-
-    Effect::new(move || {
         if let Some(msgs) = message.get() {
             set_messages.set(msgs);
             set_loading.set(false);
+            let container = messages_container
+                .get()
+                .expect("no messages container found");
+            request_animation_frame(move || {
+                let options = ScrollToOptions::new();
+                options.set_top(container.scroll_height() as f64);
+                options.set_behavior(ScrollBehavior::Smooth);
+                container.scroll_to_with_scroll_to_options(&options)
+            });
         }
-
-        debug_log!("How often");
     });
 
-    let send_message = move |_| {
-        let input = input.get();
+    let send_message = move || {
+        let input_val = input.get();
+        if input_val.is_empty() {
+            return;
+        }
 
-        send(&input);
+        send(&input_val);
+
         let mut msg_lock = set_messages.write();
         msg_lock.push(Message {
             role: "user".to_string(),
-            content: input,
+            content: input_val,
         });
         set_loading.set(true);
+        let container = messages_container
+            .get()
+            .expect("no messages container found");
+        request_animation_frame(move || {
+            let options = ScrollToOptions::new();
+            options.set_top(container.scroll_height() as f64);
+            options.set_behavior(ScrollBehavior::Smooth);
+            container.scroll_to_with_scroll_to_options(&options)
+        });
+
+        input.set("".to_string());
     };
+    let send_message = StoredValue::new(send_message);
 
     view! {
         <div id="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-circle-icon lucide-message-circle"><path d="M2.992 16.342a2 2 0 0 1 .094 1.167l-1.065 3.29a1 1 0 0 0 1.236 1.168l3.413-.998a2 2 0 0 1 1.099.092 10 10 0 1 0-4.777-4.719"/></svg></div>
@@ -118,7 +139,10 @@ fn HomePage() -> impl IntoView {
                     id="status"
                 ></div>
             </div>
-            <div id="messages">
+            <div
+                node_ref=messages_container
+                id="messages"
+            >
                 <For
                     each=move || messages.get().into_iter().enumerate()
                     key=|msg| msg.0
@@ -149,12 +173,24 @@ fn HomePage() -> impl IntoView {
             <div id="inputs">
                 <textarea
                     bind:value=input
+                    on:keypress=move |kb_event| {
+                            if kb_event.key() == "Enter".to_string() {
+                               send_message.get_value()()
+                            }
+                        }
+                    placeholder="Message..."
                     id="text"
                 ></textarea>
-                <button
-                    on:click=send_message
-                    id="send"
-                ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send-icon lucide-send"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"/><path d="m21.854 2.147-10.94 10.939"/></svg></button>
+                <Show
+                    when=move || !input.get().is_empty()
+                >
+                    <button
+                        on:click=move |_| send_message.get_value()()
+                        id="send"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send-icon lucide-send"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"/><path d="m21.854 2.147-10.94 10.939"/></svg>
+                    </button>
+                </Show>
             </div>
         </div>
     }
